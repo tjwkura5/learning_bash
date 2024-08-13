0bbd57e46438d927da655ad9c839f75d10821b91
Terry Woolard <terrywoolard@Terrys-MBP.attlocal.net>
2024-08-13 18:15:58 -0400

    directory  check

---
+++ direct_check.sh
@@ -0,0 +1,8 @@
+current_directory=$(pwd)
+
+if [ ! -d "$current_directory/backup" ]; then
+  mkdir "$current_directory/backup"
+  echo "Directory created: backup"
+else
+  echo "Directory already exists: backup"   
+fi
