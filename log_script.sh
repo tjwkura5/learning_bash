ce64b1a6fa69dc1b113f205d77e7917ea0acbc7d
Terry Woolard <terrywoolard@Terrys-MBP.attlocal.net>
2024-08-14 21:51:10 -0400

    The sort is needed to count the occurences

--- log_script.sh
+++ log_script.sh
@@ -6,13 +6,18 @@ source_file="/home/ubuntu/web-server-access-logs.log"
 
 # Use grep to filter lines containing the pattern 'HTTP/1.1"" 404' from the source file
 # This pattern matches HTTP requests that returned a 404 status code
-grep 'HTTP/1.1"" 404' "$source_file" |
+grep ' 404 ' "$source_file" |
 
 # Use awk to extract the second field from the matching lines, which represents the IP address
 # -F '[ ,"]+' sets the field separator to spaces, commas, or quotes
 awk -F '[ ,"]+' '{print $2}' |
 
+# Sort the IP addresses to group identical ones together
+# This step is necessary for uniq to count occurrences correctly
+sort |
+
 # Use uniq with the -c option to count the occurrences of each unique IP address
 # This gives the count of how many times each IP address encountered a 404 error
 uniq -c
+
 
