358c70984f555da3eaf28f08bed43c0b83dd168e
Terry Woolard <terrywoolard@Terrys-MBP.attlocal.net>
2024-08-20 17:27:55 -0400

    updates

--- permission_check.sh
+++ permission_check.sh
@@ -5,8 +5,7 @@ echo "Please provide the path to your file"
 read file
 
 if [ -f "$file" ]; then
-    file_permissions=$(stat -c %A $file| awk -F '-' '{print $2}')
-    if [[ $file_permissions =~ "rwxr" ]]; then
+    if [ -r "$file" ] && [ -w "$file" ] && [ -x "$file" ]; then
         echo "The file is readable, writable, and executable"
     else
         echo "The file is not readable, writable, and executable"
